7f1f3e4cfb1235cef971fe860184ce06dfb289e5
Michael Dadzie <dadziemikke2@gmail.com>
2020-10-20 22:50:18 +0000

    Improve UI code

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class MainDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: DrawerHeader(),
+    );
   }
 }
