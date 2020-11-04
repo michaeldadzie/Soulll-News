be8c8c922435f8aae1b4a749d135e9c5bf2a4916
Michael Dadzie <dadziemikke2@gmail.com>
2020-11-04 14:04:44 +0000

    Added Category News Widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:news_app/helpers/data.dart';
+import 'package:news_app/helpers/news.dart';
+import 'package:news_app/models/category.dart';
+import 'package:news_app/screens/category_news_screen.dart';
+import 'package:news_app/widgets/category_card.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _loading;
+  var newsList;
+
+  List<CategoryModel> categories = List<CategoryModel>();
+
+  void getNews() async {
+    News news = News();
+    await news.getNews();
+    newsList = news.news;
+    setState(() {
+      _loading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    _loading = true;
+    super.initState();
+
+    categories = getCategories();
+    getNews();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: _loading
+          ? Center(
+              child: CircularProgressIndicator(
+                backgroundColor: Theme.of(context).hintColor,
+              ),
+            )
+          : SingleChildScrollView(
+              child: Container(
+                child: Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      height: 70,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: categories.length,
+                        itemBuilder: (context, index) {
+                          return CategoryCard(
+                            imageAssetUrl: categories[index].imageAssetUrl,
+                            categoryName: categories[index].categoryName,
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                //News Articles
+              ),
+            ),
+    );
   }
 }
---
+++ lib/widgets/category_card.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:news_app/screens/category_news_screen.dart';
+
+class CategoryCard extends StatelessWidget {
+  final String imageAssetUrl, categoryName;
+
+  CategoryCard({this.imageAssetUrl, this.categoryName});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => CategoryNews(
+              newsCategory: categoryName.toString(),
+            ),
+          ),
+        );
+      },
+      child: Container(
+        margin: EdgeInsets.only(right: 14),
+        child: Stack(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(5),
+              child: CachedNetworkImage(
+                imageUrl: imageAssetUrl,
+                height: 60,
+                width: 120,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              height: 60,
+              width: 120,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.black26),
+              child: Text(
+                categoryName,
+                textAlign: TextAlign.center,
+                style: GoogleFonts.raleway(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
