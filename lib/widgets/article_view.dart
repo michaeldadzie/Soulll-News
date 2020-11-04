e2f33197d63c85f106ee37e2ab754419fdc0ef15
Michael Dadzie <dadziemikke2@gmail.com>
2020-11-04 14:08:40 +0000

    Added News Tile and Article View Widgets

---
+++ lib/widgets/article_view.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class ArticleView extends StatefulWidget {
+  final String postUrl;
+  ArticleView({this.postUrl});
+  @override
+  _ArticleViewState createState() => _ArticleViewState();
+}
+
+class _ArticleViewState extends State<ArticleView> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: WebView(
+          initialUrl: widget.postUrl,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller.complete(webViewController);
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/news_tile.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:news_app/widgets/article_view.dart';
+
+class NewsTile extends StatelessWidget {
+  final String imgUrl, title, desc, content, postUrl;
+
+  NewsTile(
+      {this.imgUrl,
+      this.desc,
+      this.title,
+      this.content,
+      @required this.postUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ArticleView(
+              postUrl: postUrl,
+            ),
+          ),
+        );
+      },
+      child: Container(
+          margin: EdgeInsets.only(bottom: 24),
+          width: MediaQuery.of(context).size.width,
+          child: Container(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              alignment: Alignment.bottomCenter,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(6),
+                      bottomLeft: Radius.circular(6))),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ClipRRect(
+                      borderRadius: BorderRadius.circular(6),
+                      child: Image.network(
+                        imgUrl,
+                        height: 200,
+                        width: MediaQuery.of(context).size.width,
+                        fit: BoxFit.cover,
+                      )),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    title,
+                    maxLines: 2,
+                    style: GoogleFonts.raleway(
+                        color: Theme.of(context).accentColor,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                    desc,
+                    maxLines: 2,
+                    style: TextStyle(color: Colors.grey, fontSize: 14),
+                  )
+                ],
+              ),
+            ),
+          )),
+    );
+  }
+}
