a2ff5c3e97ef3bab0080b5f9a6df6e5f142aee62
Michael Dadzie <dadziemikke2@gmail.com>
2020-11-06 10:42:56 +0000

    Added Share News Article Logic

--- lib/widgets/article_view.dart
+++ lib/widgets/article_view.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:share/share.dart';
 
 class ArticleView extends StatefulWidget {
   final String postUrl;
@@ -12,19 +13,37 @@ class ArticleView extends StatefulWidget {
 class _ArticleViewState extends State<ArticleView> {
   final Completer<WebViewController> _controller =
       Completer<WebViewController>();
+  bool isScrollingDown = false;
+
   @override
   Widget build(BuildContext context) {
+    String url = widget.postUrl;
+    String subject = "Share";
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: WebView(
-          initialUrl: widget.postUrl,
-          onWebViewCreated: (WebViewController webViewController) {
-            _controller.complete(webViewController);
-          },
+      body: SafeArea(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: WebView(
+            initialUrl: widget.postUrl,
+            onWebViewCreated: (WebViewController webViewController) {
+              _controller.complete(webViewController);
+            },
+          ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).primaryColor,
+        child: Icon(
+          Icons.share,
+          color: Theme.of(context).hintColor,
+        ),
+        onPressed: () {
+          final RenderBox box = context.findRenderObject();
+          Share.share(url,
+              subject: subject,
+              sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size);
+        },
       ),
     );
   }
