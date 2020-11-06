915d8d3d6b3e4de219145344b01418714ff98608
Michael Dadzie <dadziemikke2@gmail.com>
2020-11-06 10:46:41 +0000

    State Management

--- lib/helpers/news.dart
+++ lib/helpers/news.dart
@@ -1,9 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:news_app/models/article.dart';
 import 'dart:convert';
 import 'package:news_app/secret.dart';
 
-class News {
+class News with ChangeNotifier {
   List<Article> news = [];
   Future<void> getNews() async {
     String url =
@@ -13,21 +14,25 @@ class News {
 
     var jsonData = jsonDecode(response.body);
 
-    if (jsonData['status'] == "ok") {
-      jsonData["articles"].forEach((element) {
-        if (element['urlToImage'] != null && element['description'] != null) {
-          Article article = Article(
-            title: element['title'],
-            author: element['author'],
-            description: element['description'],
-            urlToImage: element['urlToImage'],
-            publishedAt: DateTime.parse(element['publishedAt']),
-            content: element["content"],
-            articleUrl: element["url"],
-          );
-          news.add(article);
-        }
-      });
+    try {
+      if (jsonData['status'] == "ok") {
+        jsonData["articles"].forEach((element) {
+          if (element['urlToImage'] != null && element['description'] != null) {
+            Article article = Article(
+              title: element['title'],
+              author: element['author'],
+              description: element['description'],
+              urlToImage: element['urlToImage'],
+              publishedAt: DateTime.parse(element['publishedAt']),
+              content: element["content"],
+              articleUrl: element["url"],
+            );
+            news.add(article);
+          }
+        });
+      }
+    } catch (error) {
+      print(error);
     }
   }
 }
@@ -43,21 +48,25 @@ class NewsForCategories {
 
     var jsonData = jsonDecode(response.body);
 
-    if (jsonData['status'] == "ok") {
-      jsonData["articles"].forEach((element) {
-        if (element['urlToImage'] != null && element['description'] != null) {
-          Article article = Article(
-            title: element['title'],
-            author: element['author'],
-            description: element['description'],
-            urlToImage: element['urlToImage'],
-            publishedAt: DateTime.parse(element['publishedAt']),
-            content: element["content"],
-            articleUrl: element["url"],
-          );
-          news.add(article);
-        }
-      });
+    try {
+      if (jsonData['status'] == "ok") {
+        jsonData["articles"].forEach((element) {
+          if (element['urlToImage'] != null && element['description'] != null) {
+            Article article = Article(
+              title: element['title'],
+              author: element['author'],
+              description: element['description'],
+              urlToImage: element['urlToImage'],
+              publishedAt: DateTime.parse(element['publishedAt']),
+              content: element["content"],
+              articleUrl: element["url"],
+            );
+            news.add(article);
+          }
+        });
+      }
+    } catch (error) {
+      print(error);
     }
   }
 }
