225a0712936b9b7363352dd8f5fb693bf19ec174
Michael Dadzie <dadziemikke2@gmail.com>
2020-12-01 22:44:48 +0000

    Redesigned ExploreScreen

--- lib/widgets/categories_explore.dart
+++ lib/widgets/categories_explore.dart
@@ -9,50 +9,68 @@ class CategoryExplore extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => CategoryNewsScreen(
-              newsCategory: categoryName.toString(),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => CategoryNewsScreen(
+                newsCategory: categoryName.toString(),
+              ),
+            ),
+          );
+        },
+        child: ListTile(
+          leading: ClipRRect(
+            borderRadius: BorderRadius.circular(5),
+            child: Container(
+              height: 40,
+              width: 40,
+              child: Image.network(
+                imageAssetUrl,
+                fit: BoxFit.contain,
+              ),
             ),
           ),
+          title: Text(
+            categoryName,
+            style: GoogleFonts.raleway(
+                color: Theme.of(context).accentColor, fontSize: 20),
+          ),
+        )
+        // Container(
+        //   margin: EdgeInsets.all(10),
+        //   child: Stack(
+        //     children: <Widget>[
+        //       ClipRRect(
+        //         borderRadius: BorderRadius.circular(5),
+        //         child: CachedNetworkImage(
+        //           imageUrl: imageAssetUrl,
+        //           height: 150,
+        //           width: MediaQuery.of(context).size.width,
+        //           fit: BoxFit.cover,
+        //         ),
+        //       ),
+        //       Container(
+        //         alignment: Alignment.center,
+        //         height: 150,
+        //         width: MediaQuery.of(context).size.width,
+        //         decoration: BoxDecoration(
+        //           borderRadius: BorderRadius.circular(5),
+        //           color: Colors.black26,
+        //         ),
+        //         child: Text(
+        //           categoryName,
+        //           textAlign: TextAlign.center,
+        //           style: GoogleFonts.raleway(
+        //             color: Colors.white,
+        //             fontSize: 30,
+        //             fontWeight: FontWeight.w300,
+        //           ),
+        //         ),
+        //       )
+        //     ],
+        //   ),
+        // ),
         );
-      },
-      child: Container(
-        margin: EdgeInsets.all(10),
-        child: Stack(
-          children: <Widget>[
-            ClipRRect(
-              borderRadius: BorderRadius.circular(5),
-              child: CachedNetworkImage(
-                imageUrl: imageAssetUrl,
-                height: 150,
-                width: MediaQuery.of(context).size.width,
-                fit: BoxFit.cover,
-              ),
-            ),
-            Container(
-              alignment: Alignment.center,
-              height: 150,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5),
-                color: Colors.black26,
-              ),
-              child: Text(
-                categoryName,
-                textAlign: TextAlign.center,
-                style: GoogleFonts.raleway(
-                  color: Colors.white,
-                  fontSize: 30,
-                  fontWeight: FontWeight.w300,
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
   }
 }
