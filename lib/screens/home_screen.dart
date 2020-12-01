14680c4b2688b43bd9d3517bdba158c0235d9688
Michael Dadzie <dadziemikke2@gmail.com>
2020-12-01 22:46:44 +0000

    Redesigned HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 import 'package:news_app/helpers/data.dart';
 import 'package:news_app/helpers/news.dart';
 import 'package:news_app/models/category.dart';
@@ -11,9 +13,9 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-Future<void> _refreshArticles(BuildContext context) async {
-  await Provider.of<News>(context, listen: false).getNews();
-}
+// Future<void> _refreshArticles(BuildContext context) async {
+//   await Provider.of<News>(context, listen: false).getNews();
+// }
 
 class _HomeScreenState extends State<HomeScreen> {
   bool _loading;
@@ -41,56 +43,88 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: _loading
-          ? Center(
-              child: CircularProgressIndicator(
-                backgroundColor: Theme.of(context).hintColor,
-              ),
-            )
-          : SingleChildScrollView(
-              physics: BouncingScrollPhysics(
-                  parent: AlwaysScrollableScrollPhysics()),
-              child: Container(
-                child: Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.symmetric(horizontal: 16),
-                      height: 70,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        physics: BouncingScrollPhysics(
-                            parent: AlwaysScrollableScrollPhysics()),
-                        itemCount: categories.length,
-                        itemBuilder: (context, index) {
-                          return CategoryCard(
-                            imageAssetUrl: categories[index].imageAssetUrl,
-                            categoryName: categories[index].categoryName,
-                          );
-                        },
+    return Scaffold(
+      body: SafeArea(
+        child: _loading
+            ? Center(
+                child: CircularProgressIndicator(
+                  backgroundColor: Theme.of(context).hintColor,
+                ),
+              )
+            : SingleChildScrollView(
+                physics: BouncingScrollPhysics(
+                    parent: AlwaysScrollableScrollPhysics()),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 5),
+                      Text(
+                        'News',
+                        style: GoogleFonts.raleway(
+                            color: Theme.of(context).accentColor,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold),
                       ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(top: 15),
-                      child: ListView.builder(
-                        itemCount: newsList.length,
-                        shrinkWrap: true,
-                        physics: ClampingScrollPhysics(),
-                        itemBuilder: (context, index) {
-                          return NewsTile(
-                            imgUrl: newsList[index].urlToImage ?? "",
-                            title: newsList[index].title ?? "",
-                            desc: newsList[index].description ?? "",
-                            content: newsList[index].content ?? "",
-                            postUrl: newsList[index].articleUrl ?? "",
-                          );
-                        },
+                      SizedBox(height: 5),
+                      Text(
+                        DateFormat.MMMMd().format(DateTime.now()),
+                        style: GoogleFonts.raleway(
+                            color: Colors.grey,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold),
                       ),
-                    )
-                  ],
+                      Divider(
+                        color: Colors.grey,
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        'Top Stories',
+                        style: GoogleFonts.raleway(
+                            color: Theme.of(context).hintColor,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 10),
+                      // Container(
+                      //   padding: EdgeInsets.symmetric(horizontal: 16),
+                      //   height: 70,
+                      //   child: ListView.builder(
+                      //     scrollDirection: Axis.horizontal,
+                      //     physics: BouncingScrollPhysics(
+                      //         parent: AlwaysScrollableScrollPhysics()),
+                      //     itemCount: categories.length,
+                      //     itemBuilder: (context, index) {
+                      //       return CategoryCard(
+                      //         imageAssetUrl: categories[index].imageAssetUrl,
+                      //         categoryName: categories[index].categoryName,
+                      //       );
+                      //     },
+                      //   ),
+                      // ),
+                      Container(
+                        child: ListView.builder(
+                          itemCount: newsList.length,
+                          shrinkWrap: true,
+                          physics: ClampingScrollPhysics(),
+                          itemBuilder: (context, index) {
+                            return NewsTile(
+                              imgUrl: newsList[index].urlToImage ?? "",
+                              title: newsList[index].title ?? "",
+                              desc: newsList[index].description ?? "",
+                              content: newsList[index].content ?? "",
+                              postUrl: newsList[index].articleUrl ?? "",
+                              publishedAt: newsList[index].publishedAt ?? "",
+                            );
+                          },
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
+      ),
     );
   }
 }
