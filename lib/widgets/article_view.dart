8941dd005a0cd2bc9cdb19964ef529dd9e25a050
Michael Dadzie <dadziemikke2@gmail.com>
2021-02-17 23:41:08 +0000

    Improve UI Code

--- lib/widgets/article_view.dart
+++ lib/widgets/article_view.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:share/share.dart';
 
@@ -25,54 +26,91 @@ class _ArticleViewState extends State<ArticleView> {
     String url = widget.postUrl;
     String subject = "Share News Article";
     return Scaffold(
-      body: Stack(
-        children: [
-          FutureBuilder(
-              future: _url,
-              builder: (BuildContext context, AsyncSnapshot snapshot) =>
-                  snapshot.hasData
-                      ? SafeArea(
-                          child: Container(
-                            height: MediaQuery.of(context).size.height,
-                            width: MediaQuery.of(context).size.width,
-                            child: WebView(
-                              initialUrl: widget.postUrl,
-                              onWebViewCreated:
-                                  (WebViewController webViewController) {
-                                _controller.complete(webViewController);
-                              },
-                            ),
-                          ),
-                        )
-                      : Center(
-                          child: CircularProgressIndicator(
-                            backgroundColor: Colors.green,
-                          ),
-                        )),
-          Positioned(
-            left: 0,
-            right: 0,
-            top: 0,
-            child: AppBar(
-              backgroundColor: Colors.transparent,
-              iconTheme: IconThemeData(color: Colors.green),
+      appBar: AppBar(
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: GestureDetector(
+              onTap: () {
+                final RenderBox box = context.findRenderObject();
+                Share.share(url,
+                    subject: subject,
+                    sharePositionOrigin:
+                        box.localToGlobal(Offset.zero) & box.size);
+              },
+              child: Text(
+                'share',
+                style: GoogleFonts.raleway(
+                    color: Theme.of(context).accentColor,
+                    fontSize: 17,
+                    fontWeight: FontWeight.w400),
+              ),
             ),
           )
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Theme.of(context).primaryColor,
-        child: Icon(
-          Icons.share,
-          color: Theme.of(context).hintColor,
-        ),
-        onPressed: () {
-          final RenderBox box = context.findRenderObject();
-          Share.share(url,
-              subject: subject,
-              sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size);
-        },
+      body: Stack(
+        children: [
+          FutureBuilder(
+            future: _url,
+            builder: (BuildContext context, AsyncSnapshot snapshot) =>
+                snapshot.hasData
+                    ? SafeArea(
+                        child: Container(
+                          height: MediaQuery.of(context).size.height,
+                          width: MediaQuery.of(context).size.width,
+                          child: WebView(
+                            initialUrl: widget.postUrl,
+                            onWebViewCreated:
+                                (WebViewController webViewController) {
+                              _controller.complete(webViewController);
+                            },
+                          ),
+                        ),
+                      )
+                    : Center(
+                        child: CircularProgressIndicator(
+                          backgroundColor: Colors.green,
+                        ),
+                      ),
+          ),
+          // Positioned(
+          //   left: 0,
+          //   right: 0,
+          //   top: 0,
+          //   child: AppBar(
+          //     backgroundColor: Colors.transparent,
+          //     iconTheme: IconThemeData(color: Colors.green),
+          //     // leading: MaterialButton(
+          //     //   child: Icon(Icons.arrow_back_ios,color: Colors.green,),
+          //     //   shape: CircleBorder(),
+          //     //   color: Colors.white,
+          //     //   elevation: 1,
+          //     // ),
+          //   ),
+          // )
+        ],
       ),
+      // floatingActionButton: FloatingActionButton(
+      //   backgroundColor: Theme.of(context).primaryColor,
+      //   child: Icon(
+      //     Icons.share,
+      //     color: Theme.of(context).hintColor,
+      //   ),
+      //   onPressed: () {
+      //     final RenderBox box = context.findRenderObject();
+      //     Share.share(url,
+      //         subject: subject,
+      //         sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size);
+      //   },
+      // ),
     );
   }
 }
