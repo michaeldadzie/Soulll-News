c9c6de076e6deeac9963e3c8eb119593cbacb357
Michael Dadzie <dadziemikke2@gmail.com>
2021-02-20 16:29:52 +0800

    Added images asset path

--- lib/models/data.dart
+++ lib/models/data.dart
@@ -6,56 +6,52 @@ List<CategoryModel> getCategories() {
 
   categoryModel = new CategoryModel();
   categoryModel.categoryName = "World";
-  categoryModel.imageAssetUrl =
-      "https://image.freepik.com/free-photo/business-people-reading-newspaper_53876-14764.jpg";
+  categoryModel.imageAssetUrl = "assets/images/world.jpg";
   myCategories.add(categoryModel);
 
   categoryModel = new CategoryModel();
   categoryModel.categoryName = "Business";
-  categoryModel.imageAssetUrl =
-      "https://cinitaslifestyle.com/wp-content/uploads/2019/09/business-3224643_1920.jpg";
+  categoryModel.imageAssetUrl = "assets/images/world.jpg";
   myCategories.add(categoryModel);
 
   categoryModel = new CategoryModel();
   categoryModel.categoryName = "Entertainment";
-  categoryModel.imageAssetUrl =
-      "https://mapworldgroup.com/wp-content/uploads/2019/10/Entertainment.jpg";
+  categoryModel.imageAssetUrl = "assets/images/entertainment.jpg";
   myCategories.add(categoryModel);
 
   categoryModel = new CategoryModel();
   categoryModel.categoryName = "Health";
-  categoryModel.imageAssetUrl = "https://eskipaper.com/images/spices-1.jpg";
+  categoryModel.imageAssetUrl = "assets/images/health.jpg";
   myCategories.add(categoryModel);
 
   categoryModel = new CategoryModel();
   categoryModel.categoryName = "Science";
-  categoryModel.imageAssetUrl = "https://wallpaperaccess.com/full/1325087.jpg";
+  categoryModel.imageAssetUrl = "assets/images/science.jpg";
   myCategories.add(categoryModel);
 
   categoryModel = new CategoryModel();
   categoryModel.categoryName = "Sports";
-  categoryModel.imageAssetUrl = "https://vistapointe.net/images/boxing-3.jpg";
+  categoryModel.imageAssetUrl = "assets/images/sports.jpg";
   myCategories.add(categoryModel);
 
   categoryModel = new CategoryModel();
   categoryModel.categoryName = "Technology";
-  categoryModel.imageAssetUrl =
-      "https://images.unsplash.com/photo-1555066931-4365d14bab8c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80";
+  categoryModel.imageAssetUrl = "assets/images/technology.jpeg";
   myCategories.add(categoryModel);
 
   categoryModel = new CategoryModel();
   categoryModel.categoryName = "Politics";
-  categoryModel.imageAssetUrl = "https://vistapointe.net/images/politics-2.jpg";
+  categoryModel.imageAssetUrl = "assets/images/politics.jpg";
   myCategories.add(categoryModel);
 
   categoryModel = new CategoryModel();
   categoryModel.categoryName = "Food";
-  categoryModel.imageAssetUrl = "https://vistapointe.net/images/food-4.jpg";
+  categoryModel.imageAssetUrl = "assets/images/food.jpg";
   myCategories.add(categoryModel);
 
   categoryModel = new CategoryModel();
   categoryModel.categoryName = "Travel";
-  categoryModel.imageAssetUrl = "https://vistapointe.net/images/travel-7.jpg";
+  categoryModel.imageAssetUrl = "assets/images/travel.jpg";
   myCategories.add(categoryModel);
 
   return myCategories;
