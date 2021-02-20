2cc2f7735bf8953914c9df0bf45a14919f2de617
Michael Dadzie <dadziemikke2@gmail.com>
2021-02-20 16:30:49 +0800

    Improve UI Code

--- lib/widgets/article_view.dart
+++ lib/widgets/article_view.dart
@@ -29,6 +29,8 @@ class _ArticleViewState extends State<ArticleView> {
       appBar: AppBar(
         elevation: 0,
         leading: IconButton(
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
           icon: Icon(Icons.arrow_back_ios),
           onPressed: () {
             Navigator.of(context).pop();
@@ -81,36 +83,8 @@ class _ArticleViewState extends State<ArticleView> {
                         ),
                       ),
           ),
-          // Positioned(
-          //   left: 0,
-          //   right: 0,
-          //   top: 0,
-          //   child: AppBar(
-          //     backgroundColor: Colors.transparent,
-          //     iconTheme: IconThemeData(color: Colors.green),
-          //     // leading: MaterialButton(
-          //     //   child: Icon(Icons.arrow_back_ios,color: Colors.green,),
-          //     //   shape: CircleBorder(),
-          //     //   color: Colors.white,
-          //     //   elevation: 1,
-          //     // ),
-          //   ),
-          // )
         ],
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   backgroundColor: Theme.of(context).primaryColor,
-      //   child: Icon(
-      //     Icons.share,
-      //     color: Theme.of(context).hintColor,
-      //   ),
-      //   onPressed: () {
-      //     final RenderBox box = context.findRenderObject();
-      //     Share.share(url,
-      //         subject: subject,
-      //         sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size);
-      //   },
-      // ),
     );
   }
 }
